df36609dbb40d92819f47e66a380bd5f43eb7b4a
dianlujitao <dianlujitao@lineageos.org>
2017-09-17 18:22:51 +0800

    X01BD: Add USB init for configfs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,7 @@ TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
